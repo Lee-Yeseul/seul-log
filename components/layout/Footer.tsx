e0433e3d1eb368d59527bc12d84ec64f86eca3e5
YESEUL LEE <95066223+Lee-Yeseul@users.noreply.github.com>
2024-02-16 15:53:51 +0900

    feature : layout 수정'

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="border-slim flex snap-start flex-col items-center justify-center gap-3 border-solid border-t-gray-50  py-5 text-sm">
-      <div className="flex gap-5">
-        <Link href="https://github.com/Lee-Yeseul"></Link>
-        <Link href="https://velog.io/@ssulv3030/posts"></Link>
-        <Link href="mailto:ssulv3030@gmail.com"></Link>
+    <footer className="w-full border-t-2 border-solid border-gray-200 py-5 text-sm">
+      <div className="flex flex-col items-center">
+        <div className="flex gap-5">
+          <Link href="https://github.com/Lee-Yeseul">github</Link>
+          <Link href="mailto:ssulv3030@gmail.com">mail</Link>
+        </div>
+        <div>©2024. YESEUL LEE. All rights reserved</div>
       </div>
-      <div>©2024. YESEUL LEE. All rights reserved</div>
     </footer>
   )
 }
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className="fixed left-0 top-0 z-10 h-16 w-full border-b-[1px] border-solid border-gray-200 bg-white">
-      <div className="flex h-full items-center justify-between px-8">
-        <div>Three의 블로그</div>
+      <div className="flex h-full items-center justify-between px-10">
+        <Link href="/">Three의 블로그</Link>
         <Link href="https://yeseul-portfolio.vercel.app/">포트폴리오</Link>
       </div>
     </header>
